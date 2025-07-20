84249faf89180ffef81d7bcc85fac32c74ba2dda
EmpiresDesMachines <empiresdesmachines@gmail.com>
2025-07-20 21:38:07 +0300

    configure database seeding

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,11 +1,19 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  Logger,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { join } from 'path';
+import { existsSync } from 'fs';
 import { readFile } from 'fs/promises';
 import { DeviceData } from './types/deviceData.interface';
 
 @Injectable()
 export class SeederService implements OnModuleInit {
+  private readonly logger = new Logger(SeederService.name);
   constructor(private readonly prisma: PrismaService) {}
   async onModuleInit() {
     await this.fillProductTable();
@@ -17,20 +25,26 @@ export class SeederService implements OnModuleInit {
       return;
     }
     const data = await this.loadData();
-    //console.log(await this.loadData());
     await this.prisma.product.createMany({ data });
   }
 
   async loadData() {
     try {
-      //const filePath = './src/seeder/data/initial-data.json';
-      const filePath = join(process.cwd(), 'src/seeder/data/initial-data.json');
+      const filePath = join(__dirname, 'data', 'initial-data.json');
+      if (!existsSync(filePath)) {
+        this.logger.error('initial-data.json not found');
+        throw new HttpException(
+          'initial-data.json not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
       const fileContent = await readFile(filePath, 'utf8');
       const data = JSON.parse(fileContent) as DeviceData[];
       return data;
     } catch (error) {
-      console.error('Ошибка при чтении JSON:', error);
-      throw error;
+      const msg = 'Failed to load initial data';
+      this.logger.error(msg);
+      throw new HttpException(msg, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
